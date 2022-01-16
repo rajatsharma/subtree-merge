adabaa2cb3f9532256f75a2c7f57c34ffc228afa
Rajat Sharma <lunasunkaiser@gmail.com>
2022-01-16 14:41:43 +0530

    Convert to Haskell

---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Control.Monad (forM_)
+import System.Environment (getArgs)
+import System.Process (callProcess)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  forM_ args $ \arg -> do
+    callProcess "git" ["subtree", "add", "--prefix=" ++ arg, "https://github.com/rajatsharma/" ++ arg, "master"]
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/rajatsharma/subtree-merge
-
-go 1.16
-
-require github.com/spf13/cobra v1.3.0 // indirect
